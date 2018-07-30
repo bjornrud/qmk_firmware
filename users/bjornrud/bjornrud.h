c272b67a303ca00f8ccd43d4df3c20d705969913
Tor Bjornrud <bjornrud@gmail.com>
2018-07-30 18:29:48 -0500

    Save for later...

--- users/bjornrud/bjornrud.h
+++ users/bjornrud/bjornrud.h
@@ -1,6 +1,14 @@
 #ifndef USERSPACE
 #define USERSPACE
 
+#define _______ KC_TRNS
+
+
+// TODO put in a keymap and get out of base userdir
+// #define DEFAULT
+// #define MAC_BASE
+// #define WIN_BASE
+
 #include "quantum.h"
 
 #endif
